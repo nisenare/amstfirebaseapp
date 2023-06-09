17539f43fe231e1fc116ed3a637d9a8de45c955e
CristinaQM <cristina.molina@easymetering.com>
2023-06-08 20:43:37 -0500

    New Functions:
    escribirTweets
    publicarTweets

--- app/src/main/java/com/example/amstfirebaseapp/perfilusuario/PerfilUsuario.java
+++ app/src/main/java/com/example/amstfirebaseapp/perfilusuario/PerfilUsuario.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.amstfirebaseapp.MainActivity;
 import com.example.amstfirebaseapp.R;
@@ -20,12 +22,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class PerfilUsuario extends AppCompatActivity {
 
-    TextView txt_id, txt_name, txt_email, txt_phone;
+    TextView txt_id, txt_name, txt_email, txt_phone, txt_tweet;
     ImageView imv_photo;
     DatabaseReference db_reference;
 
@@ -39,11 +44,13 @@ public class PerfilUsuario extends AppCompatActivity {
         txt_email = findViewById(R.id.txt_correo);
         txt_phone = findViewById(R.id.txt_phone);
         imv_photo = findViewById(R.id.imv_foto);
+        txt_tweet = findViewById(R.id.txt_tweet);
+        Intent intent = getIntent();
+        HashMap<String, String> info_user = (HashMap<String, String>) intent.getSerializableExtra("info_user");
 
         PerfilUsuarioViewModel perfilUserModel = new ViewModelProvider(this).get(PerfilUsuarioViewModel.class);
         perfilUserModel.getUiState().observe(this, userUiState -> {
-            Intent intent = getIntent();
-            HashMap<String, String> info_user = (HashMap<String, String>) intent.getSerializableExtra("info_user");
+
             Log.d("User", info_user.toString());
             txt_id.setText(info_user.get("user_id"));
             txt_name.setText(info_user.get("user_name"));
@@ -54,8 +61,36 @@ public class PerfilUsuario extends AppCompatActivity {
         });
 
         iniciarBaseDeDatos();
-        escribirTweets("Nicolas Segovia");
+        leerTweets();
+        Button btnSendTweets = findViewById(R.id.btn_enviar);
+
+        btnSendTweets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                publicarTweet(info_user.get("user_name"));
+            }
+        });
+
+        //escribirTweets("Nicolas Segovia");
     }
+
+    public void publicarTweet(String autor) {
+        // Obtén el texto del tweet desde txt_tweet
+        String tweetText = txt_tweet.getText().toString();
+
+        if (!tweetText.isEmpty()) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            String fecha = dateFormat.format(new Date());
+            escribirTweets(autor, tweetText, fecha);
+            Toast.makeText(this, "Tweet enviado correctamente", Toast.LENGTH_LONG).show();
+            txt_tweet.setText("");
+        } else {
+            Toast.makeText(this, "Ingrese un tweet", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
 
     public void cerrarSesion(View view) {
         FirebaseAuth.getInstance().signOut();
@@ -87,7 +122,19 @@ public class PerfilUsuario extends AppCompatActivity {
         );
     }
 
-    public void escribirTweets(String autor){
+    //Función nueva
+    public void escribirTweets(String autor, String tweet, String fecha) {
+        Map<String, String> hola_tweet = new HashMap<String, String>();
+        hola_tweet.put("autor", autor);
+        hola_tweet.put("fecha", fecha);
+        DatabaseReference tweets = db_reference.child("Grupo6").child("Tweets");
+        DatabaseReference nuevoTweetRef = tweets.push(); // Generar una nueva clave única
+        nuevoTweetRef.child(tweet).child("autor").setValue(autor);
+        nuevoTweetRef.child(tweet).child("fecha").setValue(fecha);
+    }
+
+    //Función hecha en clase
+    /**public void escribirTweets(String autor){
         String tweet = "hola mundo firebase por ultima vez";
         String fecha = "01-06-2023"; //Fecha actual
         Map<String, String> hola_tweet = new HashMap<String, String>();
@@ -97,5 +144,5 @@ public class PerfilUsuario extends AppCompatActivity {
         tweets.setValue(tweet);
         tweets.child(tweet).child("Autor").setValue(autor);
         tweets.child(tweet).child("Fecha").setValue(fecha);
-    }
+    }*/
 }
