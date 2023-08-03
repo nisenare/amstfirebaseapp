37aa28a7f418d22ae695455ff93826a581de4d31
CristinaQM <cristina.molina@easymetering.com>
2023-08-03 17:39:35 -0500

    lab 6 changes

---
+++ app/src/main/java/com/example/amstfirebaseapp/registros.java
@@ -0,0 +1,14 @@
+package com.example.amstfirebaseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registros);
+    }
+}
